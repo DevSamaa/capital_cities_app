c8c07a0a04b2b6b4659cb9a15e4c8ca3c78b65d6
DevSamaa <samaakanani@gmail.com>
2019-09-04 10:53:56 +1000

    I have now added a CSV file for the high score that a user can view and add their points to. It's currently just manual, I still need to figure out how to make it automatic

--- menu.rb
+++ menu.rb
@@ -2,12 +2,18 @@ def menu
     loop do
         puts " "
         puts "Welcome to the capital cities quiz."
-        puts " What would you like to do?"
+        puts " "
+        puts "What would you like to do?"
         puts "1. View High Score"
         puts "2. Start quiz"
         puts "3. Exit Quiz"
+        puts "> "
         user_menu_selection = gets.to_i
-        if user_menu_selection == 2
+        if user_menu_selection == 1
+            File.open("high_score.csv").each do |line|
+                puts line
+              end
+        elsif user_menu_selection == 2
             play_quiz
         elsif user_menu_selection == 3
             puts "Farewell dear friend, you have now left the quiz."
---
+++ quiz3.rb
@@ -0,0 +1,41 @@
+# The quiz works, now I'll try to find a way to save the high score to a CSV
+
+require_relative 'questions'
+require_relative 'menu'
+
+def play_quiz
+    counter = 0
+    QUIZ_QUESTIONS.shuffle!
+    loop do 
+        if counter == 3
+            puts "Congratulations, you just won the game"
+            puts " "
+            # add line manually to high score, change this later to do it automatically
+            File.open("high_score.csv", "w") do |line|
+                line << "Samaa, Three poings\n"
+              end
+            break
+        end 
+        quiz_item = QUIZ_QUESTIONS[counter]
+        puts " "
+        puts quiz_item[:question]
+        shuffled_cities = quiz_item[:answers].shuffle
+        shuffled_cities.each_with_index do |option, index|
+            puts "#{index +1}. #{option}"
+        end 
+        print "> "
+        user_answer = gets.chomp.to_i
+        if shuffled_cities[user_answer - 1] == QUIZ_QUESTIONS[counter][:correct]
+            puts "That's correct"
+            p "Your current score is " +"#{counter +1}"
+            puts " "
+        else
+            puts "That's incorrect. You lose."
+            puts " "
+            break
+        end
+        counter = counter + 1
+    end 
+end
+
+menu
