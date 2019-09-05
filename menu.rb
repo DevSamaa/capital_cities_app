82e709b069fa401f9c8402a35f6ff551140fed5d
DevSamaa <samaakanani@gmail.com>
2019-09-05 14:18:17 +1000

    changed the structure of the menu

--- menu.rb
+++ menu.rb
@@ -10,20 +10,20 @@ def menu_method
     puts ARTII.asciify("Quiz World")
     loop do
         puts " "
-        puts "Welcome to quiz world."
+        puts "Welcome to Quiz World."
         puts " "
         puts "What would you like to do?"
-        puts "1. View High Score"
-        puts "2. Start quiz"
+        puts "1. Start Quiz"
+        puts "2. View High Score"
         puts "3. Exit Quiz"
         puts "> "
         user_menu_selection = gets.to_i
         if user_menu_selection == 1
+            play_quiz
+        elsif user_menu_selection == 2
             File.open("high_score.csv").each do |line|
                 puts line
               end
-        elsif user_menu_selection == 2
-            play_quiz
         elsif user_menu_selection == 3
             puts ARTII.asciify("BYE BYE")
             puts "Farewell dear friend, you have now left the quiz."
